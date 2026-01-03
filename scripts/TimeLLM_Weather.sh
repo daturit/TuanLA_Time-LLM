ccda0d896e048bdcdc3e41a2080222defd3e8339
Anh Tuan <>
2026-01-03 16:05:27 +0700

    update code timeLLM_weather

--- scripts/TimeLLM_Weather.sh
+++ scripts/TimeLLM_Weather.sh
@@ -11,7 +11,9 @@ d_ff=32
 
 comment='TimeLLM-Weather'
 
-accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
+accelerate launch \
+  --num_processes 1 \
+  run_main.py \
   --task_name long_term_forecast \
   --is_training 1 \
   --root_path ./dataset/weather/ \
@@ -33,84 +35,8 @@ accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_proces
   --d_ff 32 \
   --batch_size $batch_size \
   --learning_rate $learning_rate \
-  --llm_layers $llama_layers \
+  --llm_model GPT2 \
+  --llm_dim 768 \
+  --llm_layers $llm_layers \
   --train_epochs $train_epochs \
-  --model_comment $comment
-
-accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/weather/ \
-  --data_path weather.csv \
-  --model_id weather_512_192 \
-  --model $model_name \
-  --data Weather \
-  --features M \
-  --seq_len 512 \
-  --label_len 48 \
-  --pred_len 192 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 21 \
-  --dec_in 21 \
-  --c_out 21 \
-  --d_model 32 \
-  --d_ff 32 \
-  --batch_size $batch_size \
-  --learning_rate $learning_rate \
-  --llm_layers $llama_layers \
-  --train_epochs $train_epochs \
-  --model_comment $comment
-
-  accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/weather/ \
-  --data_path weather.csv \
-  --model_id weather_512_336 \
-  --model $model_name \
-  --data Weather \
-  --features M \
-  --seq_len 512 \
-  --label_len 48 \
-  --pred_len 336 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 21 \
-  --dec_in 21 \
-  --c_out 21 \
-  --d_model 32 \
-  --d_ff 128 \
-  --batch_size $batch_size \
-  --learning_rate $learning_rate \
-  --llm_layers $llama_layers \
-  --train_epochs 10 \
-  --model_comment $comment
-
-  accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/weather/ \
-  --data_path weather.csv \
-  --model_id weather_512_720 \
-  --model $model_name \
-  --data Weather \
-  --features M \
-  --seq_len 512 \
-  --label_len 48 \
-  --pred_len 720 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 21 \
-  --dec_in 21 \
-  --c_out 21 \
-  --d_model 32 \
-  --d_ff 128 \
-  --batch_size $batch_size \
-  --learning_rate $learning_rate \
-  --llm_layers $llama_layers \
-  --train_epochs 15 \
   --model_comment $comment
