0713bd6c5d9c891b1ffd7fc5c83716fb9168a67d
Anh Tuan <>
2026-01-04 14:26:26 +0700

    update data for traffic

--- scripts/TimeLLM_Traffic.sh
+++ scripts/TimeLLM_Traffic.sh
@@ -1,7 +1,7 @@
 model_name=TimeLLM
 train_epochs=10
 learning_rate=0.01
-llama_layers=32
+llm_layers=6
 
 master_port=00097
 num_process=8
@@ -11,7 +11,9 @@ d_ff=32
 
 comment='TimeLLM-Traffic'
 
-accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
+accelerate launch \
+  --num_processes 1 \
+  run_main.py \
   --task_name long_term_forecast \
   --is_training 1 \
   --root_path ./dataset/traffic/ \
@@ -31,78 +33,80 @@ accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_proces
   --c_out 862 \
   --batch_size $batch_size \
   --learning_rate $learning_rate \
-  --llm_layers $llama_layers \
+  --llm_model GPT2 \
+  --llm_dim 768 \
+  --llm_layers $llm_layers \
   --train_epochs $train_epochs \
   --model_comment $comment
 
-accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/traffic/ \
-  --data_path traffic.csv \
-  --model_id traffic_512_96 \
-  --model $model_name \
-  --data Traffic \
-  --features M \
-  --seq_len 512 \
-  --label_len 48 \
-  --pred_len 192 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 862 \
-  --dec_in 862 \
-  --c_out 862 \
-  --batch_size $batch_size \
-  --learning_rate $learning_rate \
-  --llm_layers $llama_layers \
-  --train_epochs $train_epochs \
-  --model_comment $comment
+# accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --root_path ./dataset/traffic/ \
+#   --data_path traffic.csv \
+#   --model_id traffic_512_96 \
+#   --model $model_name \
+#   --data Traffic \
+#   --features M \
+#   --seq_len 512 \
+#   --label_len 48 \
+#   --pred_len 192 \
+#   --e_layers 2 \
+#   --d_layers 1 \
+#   --factor 3 \
+#   --enc_in 862 \
+#   --dec_in 862 \
+#   --c_out 862 \
+#   --batch_size $batch_size \
+#   --learning_rate $learning_rate \
+#   --llm_layers $llama_layers \
+#   --train_epochs $train_epochs \
+#   --model_comment $comment
 
-  accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/traffic/ \
-  --data_path traffic.csv \
-  --model_id traffic_512_96 \
-  --model $model_name \
-  --data Traffic \
-  --features M \
-  --seq_len 512 \
-  --label_len 48 \
-  --pred_len 336 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 862 \
-  --dec_in 862 \
-  --c_out 862 \
-  --batch_size 1 \
-  --learning_rate $learning_rate \
-  --llm_layers $llama_layers \
-  --train_epochs $train_epochs \
-  --model_comment $comment
+#   accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --root_path ./dataset/traffic/ \
+#   --data_path traffic.csv \
+#   --model_id traffic_512_96 \
+#   --model $model_name \
+#   --data Traffic \
+#   --features M \
+#   --seq_len 512 \
+#   --label_len 48 \
+#   --pred_len 336 \
+#   --e_layers 2 \
+#   --d_layers 1 \
+#   --factor 3 \
+#   --enc_in 862 \
+#   --dec_in 862 \
+#   --c_out 862 \
+#   --batch_size 1 \
+#   --learning_rate $learning_rate \
+#   --llm_layers $llama_layers \
+#   --train_epochs $train_epochs \
+#   --model_comment $comment
 
-  accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
-  --task_name long_term_forecast \
-  --is_training 1 \
-  --root_path ./dataset/traffic/ \
-  --data_path traffic.csv \
-  --model_id traffic_512_96 \
-  --model $model_name \
-  --data Traffic \
-  --features M \
-  --seq_len 512 \
-  --label_len 720 \
-  --pred_len 96 \
-  --e_layers 2 \
-  --d_layers 1 \
-  --factor 3 \
-  --enc_in 862 \
-  --dec_in 862 \
-  --c_out 862 \
-  --batch_size $batch_size \
-  --learning_rate $learning_rate \
-  --llm_layers $llama_layers \
-  --train_epochs $train_epochs \
-  --model_comment $comment
+#   accelerate launch --multi_gpu --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
+#   --task_name long_term_forecast \
+#   --is_training 1 \
+#   --root_path ./dataset/traffic/ \
+#   --data_path traffic.csv \
+#   --model_id traffic_512_96 \
+#   --model $model_name \
+#   --data Traffic \
+#   --features M \
+#   --seq_len 512 \
+#   --label_len 720 \
+#   --pred_len 96 \
+#   --e_layers 2 \
+#   --d_layers 1 \
+#   --factor 3 \
+#   --enc_in 862 \
+#   --dec_in 862 \
+#   --c_out 862 \
+#   --batch_size $batch_size \
+#   --learning_rate $learning_rate \
+#   --llm_layers $llama_layers \
+#   --train_epochs $train_epochs \
+#   --model_comment $comment
